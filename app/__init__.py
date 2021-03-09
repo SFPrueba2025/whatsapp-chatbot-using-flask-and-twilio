cdc5febed3b0d3805d5c9c31b456a2bde7a6b876
harry <harrisongitau3@gmail.com>
2021-03-09 15:46:03 +0300

    Add Error logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from config import Config
+import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,5 +17,26 @@ def start_ngrok():
 
 if app.config.get("ENV") == "development" and app.config["START_NGROK"]:
     start_ngrok()
+
+if not app.debug:    
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler(
+            'logs/whatsapp_chatbot.log',
+            maxBytes=10240,
+            backupCount=10
+        )
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('WhatsApp Chatbot Failure')
 
 from app import routes
--- config.py
+++ config.py
@@ -4,3 +4,5 @@ import os
 class Config(object):
     START_NGROK = os.environ.get('START_NGROK') is not None and \
         os.environ.get('WERKZEUG_RUN_MAIN') is not 'true'
+
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')

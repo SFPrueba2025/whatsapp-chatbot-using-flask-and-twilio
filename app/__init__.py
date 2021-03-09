b4104c13e2a5cb43b4a2e3a56798ad02a7a05641
harry <harrisongitau3@gmail.com>
2021-03-09 14:56:40 +0300

    Register ngrok configurations in application instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+
+def start_ngrok():
+    from pyngrok import ngrok
+
+    url = ngrok.connect(500)
+    print('* Tunnel: ', url)
+
+
+if app.config.get("ENV") == "development" and app.config["START_NGROK"]:
+    start_ngrok()
 
 from app import routes
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+
+class Config(object):
+    START_NGROK = os.environ.get('START_NGROK') is not None and \
+        os.environ.get('WERKZEUG_RUN_MAIN') is not 'true'

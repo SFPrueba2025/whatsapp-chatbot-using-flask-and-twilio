11ec6de9e5084b019d789031ef5cb140aee5bb1d
harry <harrisongitau3@gmail.com>
2021-03-19 07:06:51 +0300

    Update configurations

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config(object):
@@ -6,3 +9,11 @@ class Config(object):
         os.environ.get('WERKZEUG_RUN_MAIN') is not 'true'
 
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
+    # Email configurations
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']

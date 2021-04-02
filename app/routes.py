776658620c241bdc3805f94030f905dd7da745e0
harry <harrisongitau3@gmail.com>
2021-04-02 16:07:56 +0300

    fixing .gitignore

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,54 @@
 from app import app
-from flask import request, render_template, url_for
+from flask import request, render_template, url_for, request
 from twilio.twiml.messaging_response import MessagingResponse
 import requests
+import folium
+
+
+def get_traffic_data(lat, lon):
+    params = {'point': f'{lat},{lon}',
+              'unit': 'kmph',
+              'thickness': 14,
+              'key': app.config['TOMTOM_API_KEY']
+              }
+    base_url = 'https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json'
+    data = requests.get(base_url, params=params).json()
+    return data
+
+
+def create_reply(lat, lon):
+    data = get_traffic_data(lat, lon)
+    road_types = {'FRC0': 'Motorway',
+                  'FRC1': 'Major Road',
+                  'FRC2': 'Other major road',
+                  'FRC3': 'Secondary road',
+                  'FRC4': 'Local connecting road',
+                  'FRC5': 'Local road of high importance',
+                  'FRC6': 'Local road'
+                  }
+    if data['flowSegmentData']['roadClosure']:
+        reply = 'Unfortunately this road is closed!'
+    else:
+        reply = (f"Your nearest road is classified as a _{road_types[data['flowSegmentData']['frc']]}_.  "
+                 f"The current average speed is *{data['flowSegmentData']['currentSpeed']} kmph* and "
+                 f"would take *{data['flowSegmentData']['currentTravelTime']} seconds* to pass this section of road.  "
+                 f"With no traffic, the speed would be *{data['flowSegmentData']['freeFlowSpeed']} kmph* and would "
+                 f"take *{data['flowSegmentData']['freeFlowTravelTime']} seconds*.")
+    return reply
+
+
+@app.route('/map')
+def create_map():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+
+    data = get_traffic_data(lat, lon)
+
+    points = [(i['latitude'], i['longitude']) for i in data['flowSegmentData']['coordinates']['coordinate']]
+    m = folium.Map(location=(lat, lon), zoom_start=15)
+    folium.PolyLine(points, color='green', weight=10).add_to(m)
+
+    return m._repr_html_()
 
 
 @app.route('/')
@@ -25,9 +72,19 @@ def bot():
             quote = 'I could not retrieve a quote at this time, sorry.'
         msg.body(quote)
         responded = True
-    if 'cat' in incoming_msg:
+    elif 'cat' in incoming_msg:
         msg.media('https://cataas.com/cat')
         responded = True
+    elif 'Latitude' in request.values.get() and "Longitude" in request.values.keys():
+        lat = request.values.get('Latitude')
+        lon = request.values.get('Longitude')
+
+        reply = create_reply(lat, lon)
+        url = f'{request.url_root}map?lat={lat}&lon={lon}'
+        msg.body(f'{reply}\n\nCheck out the interactive map here:\n{url}')
+        responded = True
+
     if not responded:
         msg.body('Apologies, I do not understand what you really asked.')
+
     return str(resp)

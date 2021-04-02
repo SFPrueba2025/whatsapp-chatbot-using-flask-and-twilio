eb11b718c2cf2ebd02ed4d137618e4ae44bdf19c
harry <harrisongitau3@gmail.com>
2021-04-02 16:29:11 +0300

    Make changes for heroku deployment

--- app/routes.py
+++ app/routes.py
@@ -6,11 +6,7 @@ import folium
 
 
 def get_traffic_data(lat, lon):
-    params = {'point': f'{lat},{lon}',
-              'unit': 'kmph',
-              'thickness': 14,
-              'key': app.config['TOMTOM_API_KEY']
-              }
+    params = {'point': f'{lat},{lon}', 'unit': 'mph', 'thickness': 14, 'key': os.environ['TOMTOM_API_KEY']}
     base_url = 'https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json'
     data = requests.get(base_url, params=params).json()
     return data
@@ -18,21 +14,17 @@ def get_traffic_data(lat, lon):
 
 def create_reply(lat, lon):
     data = get_traffic_data(lat, lon)
-    road_types = {'FRC0': 'Motorway',
-                  'FRC1': 'Major Road',
-                  'FRC2': 'Other major road',
-                  'FRC3': 'Secondary road',
-                  'FRC4': 'Local connecting road',
-                  'FRC5': 'Local road of high importance',
-                  'FRC6': 'Local road'
-                  }
+
+    road_types = {'FRC0': 'Motorway', 'FRC1': 'Major road', 'FRC2': 'Other major road', 'FRC3': 'Secondary road',
+                  'FRC4': 'Local connecting road', 'FRC5': 'Local road of high importance', 'FRC6': 'Local road'}
+
     if data['flowSegmentData']['roadClosure']:
         reply = 'Unfortunately this road is closed!'
     else:
         reply = (f"Your nearest road is classified as a _{road_types[data['flowSegmentData']['frc']]}_.  "
-                 f"The current average speed is *{data['flowSegmentData']['currentSpeed']} kmph* and "
+                 f"The current average speed is *{data['flowSegmentData']['currentSpeed']} mph* and "
                  f"would take *{data['flowSegmentData']['currentTravelTime']} seconds* to pass this section of road.  "
-                 f"With no traffic, the speed would be *{data['flowSegmentData']['freeFlowSpeed']} kmph* and would "
+                 f"With no traffic, the speed would be *{data['flowSegmentData']['freeFlowSpeed']} mph* and would "
                  f"take *{data['flowSegmentData']['freeFlowTravelTime']} seconds*.")
     return reply
 
@@ -45,8 +37,11 @@ def create_map():
     data = get_traffic_data(lat, lon)
 
     points = [(i['latitude'], i['longitude']) for i in data['flowSegmentData']['coordinates']['coordinate']]
+
     m = folium.Map(location=(lat, lon), zoom_start=15)
     folium.PolyLine(points, color='green', weight=10).add_to(m)
+
+    return m._repr_html_()
 
     return m._repr_html_()
 
